c4ae671bcfc0d2256c56433780a4638d78b45d82
Shania <157773732+esnotme@users.noreply.github.com>
2025-01-27 07:38:07 -0400

    Update main.py

--- main.py
+++ main.py
@@ -2,4 +2,19 @@ x = 10 #initialization
 x = 23 #assignment
 y #declaration
 z = ( x + y)/x + (78%3_) 
+name = "bobby" #string
+age = 12 #integer
+height = 6.5 # float
+hasDate = False
+comp = 7j #complex
 
+# using fstrings for interpolation
+
+message = f'Hi my name is {name} I am {age} years old'
+print(message) # ‘Hi my name is bobby I am 12 years old'
+
+# type casting to convert types
+intHeight = int(height) # 6
+strHeight = str(height) # '6'
+floatHeight = float(intHeight) # 6.0 
+ageType = type(age)
